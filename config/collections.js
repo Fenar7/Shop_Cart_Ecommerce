2f8dcb0c29c56e1bd66b6e8aeb71257368ff4ba5
Fenar Khan <fenarkhan@gmail.com>
2024-05-17 12:22:16 +0530

    5

---
+++ config/collections.js
@@ -0,0 +1,3 @@
+module.exports={
+    PROUDUCT_COLLECTION: 'product'
+}
--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,16 +1,28 @@
-var db = require('../config/connection')
+const db = require('../config/connection');
+const collection = require('../config/collections');
 
 module.exports = {
     addProduct: (product, callback) => {
         console.log(product);
         db.get().collection('product').insertOne(product)
             .then((data) => {
-                console.log(data)
+                console.log(data);
                 callback(data.insertedId);
             })
             .catch((err) => {
                 console.error("Error occurred while adding product:", err);
                 callback(false); // Invoke callback with false to indicate failure
             });
+    },
+
+    getAllProducts: () => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let products = await db.get().collection(collection.PROUDUCT_COLLECTION).find().toArray();
+                resolve(products);
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
-}
+};
